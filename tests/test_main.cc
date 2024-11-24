c2ff517ef3c12926e381b5cefa8af6b37ff26fe7
Feglawy <a.m9518520963@outlook.com>
2024-11-24 17:45:58 +0200

    commented the main function in test file

--- tests/test_main.cc
+++ tests/test_main.cc
@@ -7,8 +7,8 @@ TEST(AdditionTest, PositiveNumbers)
     EXPECT_EQ(add(2, 3), 5);
 }
 
-int main(int argc, char **argv)
-{
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
+// int main(int argc, char **argv)
+// {
+//     ::testing::InitGoogleTest(&argc, argv);
+//     return RUN_ALL_TESTS();
+// }
