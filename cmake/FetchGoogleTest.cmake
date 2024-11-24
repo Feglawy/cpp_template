bb86ece600a402ebc4ea75fa319f0d7a3aa548c8
Feglawy <a.m9518520963@outlook.com>
2024-11-24 17:45:28 +0200

    added messages to cmake files

--- cmake/FetchGoogleTest.cmake
+++ cmake/FetchGoogleTest.cmake
@@ -2,7 +2,8 @@ include(FetchContent)
 
 # Use provided version or fallback to default
 if(NOT DEFINED GTEST_VERSION)
-    set(GTEST_VERSION "v1.15.2")
+  set(GTEST_VERSION "v1.15.2")
+  message(NOTICE "GTEST version is not specified; it will fallback to: ${GTEST_VERSION}")
 endif()
 
 FetchContent_Declare(
@@ -16,3 +17,10 @@ set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
 
 # Fetch GoogleTest
 FetchContent_MakeAvailable(googletest)
+
+# Check if the googletest content was successfully made available
+if(googletest_POPULATED)
+  message(STATUS "GoogleTest version ${GTEST_VERSION} successfully downloaded and made available.")
+else()
+  message(FATAL_ERROR "Failed to fetch GoogleTest.")
+endif()
--- cmake/config.cmake
+++ cmake/config.cmake
@@ -9,9 +9,14 @@ set(CMAKE_CXX_STANDARD ${MYPROJECT_CXX_STANDARD})
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 
 # Option to enable or disable testing
-option(ENABLE_TESTING "Enable testing support" ON)
+option(ENABLE_TESTING "Enable testing support" OFF)
 
 # Define GoogleTest version
 set(GTEST_VERSION "v1.15.2" CACHE STRING "Version of GoogleTest to fetch")
 
 
+message(STATUS "Project: ${PROJECT_NAME}")
+message(STATUS "Version: ${MYPROJECT_VERSION}")
+message(STATUS "C++ Standard: ${CMAKE_CXX_STANDARD}")
+message(STATUS "Build Type: ${CMAKE_BUILD_TYPE}")
+message(STATUS "Tests enabled: ${ENABLE_TESTING}")
