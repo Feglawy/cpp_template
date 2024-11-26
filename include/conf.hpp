cd0ca5e5824e55256bcaeafc2f0e23cb222ba18e
Feglawy <a.m9518520963@outlook.com>
2024-11-26 13:20:05 +0200

    added some commands to the conf header file

--- include/conf.hpp
+++ include/conf.hpp
@@ -1,17 +1,19 @@
 #pragma once
 #include <bits/stdc++.h>
 
-const inline std::string INPUT_FILE_PATH = "../input.txt";
-const inline std::string OUTPUT_FILE_PATH = "../output.txt";
-
+// Enables fast I/O
 #define fast std::ios_base::sync_with_stdio(false), std::cin.tie(0), std::cout.tie(0);
 
-#define read (void)freopen(INPUT_FILE_PATH, "r", stdin);
-#define write (void)freopen(OUTPUT_FILE_PATH, "w", stdout);
+// Redirects the standard input to a file
+#define read(INPUT_FILE_PATH) (void)freopen(INPUT_FILE_PATH, "r", stdin);
+
+// Redirects the standard output to a file
+#define write(OUTPUT_FILE_PATH) (void)freopen(OUTPUT_FILE_PATH, "w", stdout);
 
 #define clearConsole system("cls");
 #define pauseConsole system("pause>nul");
 
+// Make windows terminal utf-8
 #define makeConsoleUTF8 system("chcp 65001");
 
 #define all(v) (v).begin(), (v).end()
