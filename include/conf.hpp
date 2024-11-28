dbbf87f752d92be5b8ec7a6ed761892997fbc55b
Feglawy <a.m9518520963@outlook.com>
2024-11-28 17:43:55 +0200

    renamed some macros

--- include/conf.hpp
+++ include/conf.hpp
@@ -5,10 +5,10 @@
 #define fast std::ios_base::sync_with_stdio(false), std::cin.tie(0), std::cout.tie(0);
 
 // Redirects the standard input to a file
-#define read(INPUT_FILE_PATH) (void)freopen(INPUT_FILE_PATH, "r", stdin);
+#define readFromFile(INPUT_FILE_PATH) (void)freopen(INPUT_FILE_PATH, "r", stdin);
 
 // Redirects the standard output to a file
-#define write(OUTPUT_FILE_PATH) (void)freopen(OUTPUT_FILE_PATH, "w", stdout);
+#define writeToFile(OUTPUT_FILE_PATH) (void)freopen(OUTPUT_FILE_PATH, "w", stdout);
 
 #define clearConsole system("cls");
 #define pauseConsole system("pause>nul");
